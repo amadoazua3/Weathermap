219c449939fa8488f0f01689b7e6a2049354a401
Amado Azua III <amado.azua3@gmail.com>
2021-06-11 16:39:41 -0500

    Weather map project

--- js/mapbox_utils.js
+++ js/mapbox_utils.js
@@ -1,3 +1,8 @@
+// TODO: Create the map, add to #map
+// Create the geocoder and add it how you see fit
+// Set the process for creating/updating/removing markers and popups
+// Create the callback to be used to pass coordinates along to weathermap-utils.js when the response from geocoder endpoint is SUCCESSFUL
+
 const mapboxToken = MAPBOX_TOKEN;
 
 mapboxgl.accessToken = mapboxToken;
@@ -7,5 +12,26 @@ var map = new mapboxgl.Map({
     style: 'mapbox://styles/mapbox/dark-v10',
     center: [-95.3698, 29.7604],
     zoom: 12
-})
+});
+
+// Add the control to the map.
+map.addControl(
+    new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl
+    })
+);
+
+let marker;
+
+function addGeocoder(geocoder){
+
+
+    // geocoder.on('result', function (event) {
+    //
+    // });
+
+}
+
+
 
